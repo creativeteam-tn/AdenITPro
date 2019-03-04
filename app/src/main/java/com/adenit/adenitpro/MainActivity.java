4cd5521045456e453db47e708ad526a659d60a4d
Creative Team <creativeteam.tn@gmail.com>
2019-03-04 11:57:32 +0100

    This project is part of our online udemy course entitled: Android: Build Voting App using SMS and SQLite (English)

---
+++ app/src/main/java/com/adenit/adenitpro/MainActivity.java
@@ -0,0 +1,23 @@
+package com.adenit.adenitpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv=(TextView)findViewById(R.id.textView);
+
+    }
+
+    public void greet( View v){
+        tv.setText("My First App");
+    }
+}
