04f8c95bf49b0def515bc20b1675609d2ecdf4c5
Creative Team <creativeteam.tn@gmail.com>
2019-03-07 13:11:33 +0100

    Part2 corresponding to video 8

--- app/src/main/java/com/adenit/adenitpro/ReceiverAct.java
+++ app/src/main/java/com/adenit/adenitpro/ReceiverAct.java
@@ -1,13 +1,20 @@
 package com.adenit.adenitpro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiverAct extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
+
+        tv=(TextView)findViewById(R.id.tvWelcome);
+        Intent i = getIntent();
+        tv.setText("Welcome: "+ i.getStringExtra("name"));
     }
 }
--- app/src/main/java/com/adenit/adenitpro/SenderActivity.java
+++ app/src/main/java/com/adenit/adenitpro/SenderActivity.java
@@ -5,17 +5,22 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SenderActivity extends AppCompatActivity {
 
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender);
+
+        et= (EditText)findViewById(R.id.etName);
     }
 
     public void goToAct(View view) {
         Intent i= new Intent(SenderActivity.this, ReceiverAct.class);
+        i.putExtra("name",et.getText().toString());
         startActivity(i);
     }
 
