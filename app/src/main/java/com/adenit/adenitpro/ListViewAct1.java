09575326f847002857b7d0d443369a0a69e2af68
Creative Team <creativeteam.tn@gmail.com>
2019-03-07 19:40:37 +0100

    Part2 corresponding to video 10

--- app/src/main/java/com/adenit/adenitpro/ListViewAct1.java
+++ app/src/main/java/com/adenit/adenitpro/ListViewAct1.java
@@ -2,8 +2,11 @@ package com.adenit.adenitpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,9 +37,24 @@ public class ListViewAct1 extends AppCompatActivity {
         books.add("SQLite");
         books.add("GCM");
 
-        ArrayAdapter<String> adt= new ArrayAdapter<String>(
+        final ArrayAdapter<String> adt= new ArrayAdapter<String>(
                 ListViewAct1.this, android.R.layout.simple_list_item_1,books);
         lv.setAdapter(adt);
-    }
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewAct1.this, "You have clicked "+
+                        adt.getItem(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                adt.remove(adt.getItem(position));
+                return false;
+            }
+        });}
 }
 
